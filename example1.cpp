eb25ce98cb905b725358382a2356ff7aa7ffa8c4
Wadie Mendja <mendja2014@gmail.com>
2021-04-29 21:18:19 +0100

    commit

---
+++ example1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Hello World!" << endl;
+        string a = "Hello";
+        cout << a + "Wadie";
+    }
+    return 0;
+}
